3f86f9c9b84c7c9fcbde83ab71fe959ebf6cf973
mrazi25 <114091178+mrazi25@users.noreply.github.com>
2024-01-08 18:58:08 +0700

    update

--- main/Home.py
+++ main/Home.py
@@ -4,22 +4,20 @@ import streamlit as st
 import base64
 
 
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
-
-
-autoplay_audio("/Users/family/Documents/GitHub/ICU-Deployment/main/assets/background-music.mp3")
+#def autoplay_audio(file_path: str):
+#    with open(file_path, "rb") as f:
+#        data = f.read()
+#        b64 = base64.b64encode(data).decode()
+#        md = f"""
+#            <audio autoplay="true">
+#            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+#            </audio>
+#            """
+#        st.markdown(
+#            md,
+#            unsafe_allow_html=True,
+#        )
+#autoplay_audio("/Users/family/Documents/GitHub/ICU-Deployment/main/assets/background-music.mp3")
 st.title("Sepvisor")
 
 # Project description
--- main/pages/Demo.py
+++ main/pages/Demo.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle as pkl
 import pandas as pd
+from itertools import product
 
 with open('/Users/family/Documents/GitHub/ICU-Deployment/What I need. maybe?/dqn_normal_actions_test.p', 'rb') as file:
     agent_action_test = pkl.load(file)
@@ -15,7 +16,7 @@ data = pd.read_csv('/Users/family/Documents/GitHub/ICU-Deployment/What I need. m
 
 # Page title
 st.title("Demo")
-
+st.divider()
 # Introduction
 st.write(
     "This page demonstrates a simple prediction using a hypothetical machine learning model. "
@@ -27,12 +28,13 @@ selected_option = st.selectbox("Select an ICU Stay ID:", options)
 
 # Display the selected option
 st.write(f"You selected: {selected_option}")
-
+st.divider()
 #def show_table(selected_state):
 
 
 # Button to trigger prediction
 if st.button("Predict"):
+    st.subheader("Dose Intensity Recomendation for The Patients")
     save_indexes = []
     i_index = 0
     for i, row in data.iterrows():
@@ -41,64 +43,49 @@ if st.button("Predict"):
     
     phys_act = []
     ai_act = []
-    #ivfluid_dose = ['0', '>0-40', '40-205', '205-635', '>635']
-    #vaso_dose = ['0', '>0-0.110', '0.110-0.225', '0.225-0.450', '>0.450']
-    actions = ['0 ml dose of iv fluid and 0 ug/kg/h dose of vasopressor',
-              '0 ml dose of iv fluid and >0-0.110 ug/kg/h dose of vasopressor',
-              '0 ml dose of iv fluid and 0.110-0.225 ug/kg/h dose of vasopressor',
-              '0 ml dose of iv fluid and 0.225-0.450 ug/kg/h dose of vasopressor',
-              '0 ml dose of iv fluid and >0.450 ug/kg/h dose of vasopressor',
-              '>0-40 ml dose of iv fluid and 0 ug/kg/h dose of vasopressor',
-              '>0-40 ml dose of iv fluid and >0-0.110 ug/kg/h dose of vasopressor',
-              '>0-40 ml dose of iv fluid and 0.110-0.225 ug/kg/h dose of vasopressor',
-              '>0-40 ml dose of iv fluid and 0.225-0.450 ug/kg/h dose of vasopressor',
-              '>0-40 ml dose of iv fluid and >0.450 ug/kg/h dose of vasopressor',
-              '40-205 ml dose of iv fluid and 0 ug/kg/h dose of vasopressor',
-              '40-205 ml dose of iv fluid and >0-0.110 ug/kg/h dose of vasopressor',
-              '40-205 ml dose of iv fluid and 0.110-0.225 ug/kg/h dose of vasopressor',
-              '40-205 ml dose of iv fluid and 0.225-0.450 ug/kg/h dose of vasopressor',
-              '40-205 ml dose of iv fluid and >0.450 ug/kg/h dose of vasopressor',
-              '205-635 ml dose of iv fluid and 0 ug/kg/h dose of vasopressor',
-              '205-635 ml dose of iv fluid and >0-0.110 ug/kg/h dose of vasopressor',
-              '205-635 ml dose of iv fluid and 0.110-0.225 ug/kg/h dose of vasopressor',
-              '205-635 ml dose of iv fluid and 0.225-0.450 ug/kg/h dose of vasopressor',
-              '205-635 ml dose of iv fluid and >0.450 ug/kg/h dose of vasopressor',
-              '>635 ml dose of iv fluid and 0 ug/kg/h dose of vasopressor',
-              '>635 ml dose of iv fluid and >0-0.110 ug/kg/h dose of vasopressor',
-              '>635 ml dose of iv fluid and 0.110-0.225 ug/kg/h dose of vasopressor',
-              '>635 ml dose of iv fluid and 0.225-0.450 ug/kg/h dose of vasopressor',
-              '>635 ml dose of iv fluid and >0.450 ug/kg/h dose of vasopressor',]
+    vaso_phys = []
+    iv_phys = []
+    vaso_ai = []
+    iv_ai = []
+    doses_intensity = ["Zero", "Low", "Medium", "High", "Very High"]
+    pair_of_act = list(product(doses_intensity, repeat=2))
+    st.divider()
+    # Your loop to fill the data
     for i in range(len(save_indexes)):
-        phys_act.append(phys_action_test[save_indexes[i]])
-        st.write(f"Physician action is giving {actions[phys_act[i]]}")
-        ai_act.append(agent_action_test[save_indexes[i]])
-        st.write(f"AI action is giving {actions[ai_act[i]]}")
-        st.write("=====================================================================================\n")
+        st.text(f"Dose Intensity After {(i+1)*4} Hour")
+        phys_act.append(pair_of_act[phys_action_test[save_indexes[i]]])
+        ai_act.append(pair_of_act[agent_action_test[save_indexes[i]]])
+        vaso_phys.append(phys_act[i][0])
+        iv_phys.append(phys_act[i][1])
+        vaso_ai.append(ai_act[i][0])
+        iv_ai.append(ai_act[i][1])
+        temp = {
+            'IV Fluid': {'Physician': phys_act[i][0], 'AI': ai_act[i][0]},
+            'Vasopressor': {'Physician': phys_act[i][1], 'AI': ai_act[i][1]}
+        }
+        df = pd.DataFrame(temp)
+        st.table(df)
+        st.divider()
 
     # Data
-    x = range(len(ai_act))  # Assume ai_act and phys_act have the same length
-    y1 = ai_act
-    y2 = phys_act
+    #doses_intensity_set1 = ['High', 'Low', 'Zero', 'High', 'High', 'Low']
+    #doses_intensity_set2 = ['Medium', 'High', 'Low', 'Medium', 'High', 'Medium']
+    y_labels = ["Zero", "Low", "Medium", "High", "Very High"]
 
-    # Plotting garis
-    plt.plot(x, y1, label='AI', marker='o')
-    plt.plot(x, y2, label='Physician', marker='o')
+    # Map doses_intensity to corresponding numerical values
+    y_values_set1 = [y_labels.index(intensity) for intensity in vaso_phys]
+    y_values_set2 = [y_labels.index(intensity) for intensity in vaso_ai]
 
-    # Adding a y-axis range up to 24
-    plt.ylim(0, 24)
+    # Create a line plot for each set
+    plt.plot(range(len(vaso_phys)), y_values_set1, label='Physician', marker='o', linestyle='-')
+    plt.plot(range(len(vaso_ai)), y_values_set2, label='AI', marker='o', linestyle='-')
 
-    # Adding labels and title
-    plt.xlabel('State')
-    plt.ylabel('Choosen Action ')
-    plt.title('Dose of IV fluid & Vasopressor Combination')
-
-    # Adding a legend
+    # Customize the plot
+    plt.xlabel('Index 4-Hourly Timestamps')
+    plt.ylabel('Intensity')
+    plt.title('Vasopressor Doses Intensity Line Plot')
+    plt.yticks(range(len(y_labels)), y_labels)
     plt.legend()
-
-    # Adding a grid
-    plt.grid(True)
-
-    # Displaying the plot using st.pyplot
     st.pyplot(plt)
 
     #show_sofa = st.checkbox("SOFA Score")
@@ -107,8 +94,22 @@ if st.button("Predict"):
     selected_param = ["SOFA", "GCS", "SIRS", "Temp_C", "SysBP", "DiaBP", "HR"]
     show_table = data[selected_param]
     show_table = show_table.loc[save_indexes].reset_index(drop=True)
-    st.write("Collection of patient states")
-    st.table(show_table)
+    plt.figure(figsize=(10, 6))
+    plt.plot(show_table['SOFA'], label='SOFA')
+    plt.plot(show_table['GCS'], label='GCS')
+    plt.plot(show_table['SIRS'], label='SIRS')
+    plt.plot(show_table['Temp_C'], label='Temperature (C)')
+    plt.plot(show_table['SysBP'], label='SysBP')
+    plt.plot(show_table['DiaBP'], label='DiaBP')
+    plt.plot(show_table['HR'], label='Heart Rate')
+
+    # Customize the plot
+    plt.xlabel('Time')
+    plt.ylabel('Normalized Values')
+    plt.title('Parameters Over Time')
+    plt.legend()
+    plt.grid(True)
+    st.pyplot(plt)
     #if show_sofa:
     #    selected_param.append("SOFA")
     #else:
